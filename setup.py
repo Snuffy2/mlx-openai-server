cb7858556067247b7ed54162391be4b9ad8b572b
Snuffy2 <Snuffy2@users.noreply.github.com>
2025-11-11 14:17:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_packages
-from app import __version__
+from setuptools import find_packages, setup
 
+from app import __version__
 
 setup(
     name="mlx-openai-server",
@@ -11,7 +11,7 @@ setup(
     description="A high-performance API server that provides OpenAI-compatible endpoints for MLX models. Built with Python and FastAPI, it enables efficient, scalable, and user-friendly local deployment of MLX-based multimodal models with an OpenAI-compatible interface. Supports text, vision, and audio processing capabilities. Perfect for developers looking to run MLX models locally while maintaining compatibility with existing OpenAI-based applications.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    packages=find_packages(), 
+    packages=find_packages(),
     install_requires=[
         "mlx-vlm==0.3.5",
         "mlx-lm==0.28.3",
@@ -28,7 +28,7 @@ setup(
         "librosa==0.11.0",
         "openai-harmony==0.0.4",
         "json_repair==0.52.1",
-        "python-multipart==0.0.20"
+        "python-multipart==0.0.20",
     ],
     extras_require={
         "dev": [
@@ -40,7 +40,7 @@ setup(
     },
     entry_points={
         "console_scripts": [
-            "mlx-openai-server=app.cli:cli",
+            "mlx-openai-server = app.main:main",
         ],
     },
     python_requires=">=3.11",
@@ -49,4 +49,4 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-) 
+)
